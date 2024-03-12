8df8d2f104726a1e055c065e6692bd6a81d29d12
Dudisdu99 <131018122+Dudisdud99@users.noreply.github.com>
2024-03-12 22:16:59 +0000

    iniciando projeto

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector('nav'); 
+
+nav.innerHTML = `
+    <div class="logo">
+        <div class="nomeLogo">SIMULANDO</div>
+    </div>
+    <div class="menu">
+        <div class="perfil"></div>
+        <div class="perfil"></div>
+        <div class="perfil"></div>
+    </div>
+    `
