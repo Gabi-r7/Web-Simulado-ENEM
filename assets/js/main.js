755fe34fc5506d86974cca33d3dba60b238dac9a
Dudisdu99 <131018122+Dudisdud99@users.noreply.github.com>
2024-03-13 19:20:45 +0000

    fazendo carrocel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-const nav = document.querySelector('nav'); 
+// navegador
+
+const nav = document.querySelector('nav');
 
 nav.innerHTML = `
     <div class="logo">
@@ -8,5 +10,50 @@ nav.innerHTML = `
         <div class="perfil"></div>
         <div class="perfil"></div>
         <div class="perfil"></div>
-    </div>
-    `
+    </div>`
+
+// carrocel
+
+const carrocel = document.querySelector('.carrocel');
+
+let estiloElemento = getComputedStyle(document.documentElement);
+let gapCaixas = estiloElemento.getPropertyValue('--gap-caixas-carrocel');
+
+let larguraCaixa = carrocel.getBoundingClientRect().width;
+let quantidadeCaixas = carrocel.children.length;
+
+function moverCarrocel(direcao) {
+    let larguraCarrocel = carrocel.getBoundingClientRect().width;
+    console.log(larguraCarrocel);
+
+    let posicaoAtual = carrocel.scrollLeft;
+    console.log(posicaoAtual);
+
+    let novaPosicao;
+
+    const mover = (caixaWidth + espacoEntreCaixas) * elementosParaMover; //fafafaffafffaffafaf
+
+    if (direcao === 1) {
+        console.log('proximo!!!');
+        novaPosicao = posicaoAtual + larguraCarrocel;
+    }
+    else {
+        console.log('volta ae!!!');
+        novaPosicao = posicaoAtual - larguraCarrocel;
+    }
+
+    carrocel.scrollTo({
+        left: novaPosicao,
+        behavior: 'smooth'
+    });
+}
+
+// Carrocel Eventos
+
+document.querySelector('.proximo').addEventListener('click', function () {
+    moverCarrocel(1);
+});
+
+document.querySelector('.anterior').addEventListener('click', function () {
+    moverCarrocel(-1);
+});
