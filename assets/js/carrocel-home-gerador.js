aa96fd93f4aa5750261df334468368ad89648e7c
Dudisdu99 <131018122+Dudisdud99@users.noreply.github.com>
2024-04-02 18:12:18 +0000

    funciona o carrocel

--- assets/js/carrocel-home-gerador.js
+++ assets/js/carrocel-home-gerador.js
@@ -1,13 +1,10 @@
 const carrocelInicial = document.querySelector('.carrocel');
-let quantiaCaixa = 5;
+let quantiaCaixa = 3;
 
 for(let i = 0; i < quantiaCaixa; i++){
-    let novaCaixa = document.createElement('div');
-    novaCaixa.className = 'caixa';
-    
-    let novaModalidade = document.createElement('div');
-    novaModalidade.className = 'modalidade';
-    
-    novaCaixa.appendChild(novaModalidade);
-    carrocelInicial.appendChild(novaCaixa);
+    carrocelInicial.innerHTML += `
+        <div class="caixa">
+            <div class="modalidade"></div>
+        </div>
+    `;
 }
