bf38f11a0965a9460c80e84295cd37fc93a15727
Gabriel Henrique Lamb <120254391+Gabi-r7@users.noreply.github.com>
2024-05-14 14:28:00 +0000

    carregando página

--- assets/js/perguntas.js
+++ assets/js/perguntas.js
@@ -8,6 +8,10 @@ carregarPerguntas(ano, tipo);
 let perguntas = null;
 let indicePerguntaAtual = 0;
 
+window.onload = function() {
+    document.getElementById('loading').style.display = 'none';
+};
+
 function embaralharArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
