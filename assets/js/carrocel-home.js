d4fd599a442eb5a20103e50897177bf018d3de84
Dudisdu99 <131018122+Dudisdud99@users.noreply.github.com>
2024-06-04 18:07:17 +0000

    criando novo carrocel e arrumando css

--- assets/js/carrocel-home.js
+++ assets/js/carrocel-home.js
@@ -1,58 +1,20 @@
-// carrocel
+let pontos = document.querySelectorAll(".ponto");
+let caixas = document.querySelectorAll(".caixa");
+let indiceAtual = 0;
 
-const carrocel = document.querySelector('.carrocel');
-const caixa = document.querySelector('.caixa'); 
-const proximo = document.querySelector('.proximo'); 
-const anterior = document.querySelector('.anterior'); 
-
-anterior.style.display = 'none';
-
-let quantiaCaixa = 5;
-
-let larguraCaixa = caixa.offsetWidth;
-
-function moverCarrocel(direcao) {
-    
-    let posicaoAtual = carrocel.scrollLeft;
-    console.log(posicaoAtual);
-    
-    let novaPosicao;
-    
-    let quantiaMover = larguraCaixa ;
-
-    if (direcao === 1) {
-        if(posicaoAtual >= larguraCaixa * (quantiaCaixa -2)){
-            proximo.style.display = 'none';
-        }
-        if(posicaoAtual == 0){
-            anterior.style.display = 'grid';
-        }
-        console.log('proximo!!!');
-        novaPosicao = posicaoAtual + quantiaMover;
-    }
-    else {
-        if(posicaoAtual >= larguraCaixa * (quantiaCaixa -2)){
-            proximo.style.display = 'grid';
-        }
-        if(posicaoAtual >= larguraCaixa && posicaoAtual < larguraCaixa*2){
-            anterior.style.display = 'none';
-        }
-        console.log('volta ae!!!');
-        novaPosicao = posicaoAtual - quantiaMover;
-    }
-
-    carrocel.scrollTo({
-        left: novaPosicao,
-        behavior: 'smooth'
-    });
+function atualizarDiv() {
+    caixas.forEach(caixa => caixa.classList.remove('ativo'));
+    pontos.forEach(ponto => ponto.classList.remove('ativo'));
+    caixas[indiceAtual].classList.add("ativo");
+    pontos[indiceAtual].classList.add("ativo");
 }
 
-// Carrocel Eventos
-
-document.querySelector('.proximo').addEventListener('click', function () {
-    moverCarrocel(1);
+pontos.forEach((ponto, indice) => {
+    ponto.addEventListener("click", () => {
+        indiceAtual = indice;
+        console.log(indiceAtual); 
+        atualizarDiv();
+    });
 });
 
-document.querySelector('.anterior').addEventListener('click', function () {
-    moverCarrocel(-1);
-});
+atualizarDiv();
