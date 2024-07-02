50717cb6a4148f4bf7da04a63034a3266308ed9d
Dudisdu99 <131018122+Dudisdud99@users.noreply.github.com>
2024-07-02 18:07:08 +0000

    fazendo css de verdade

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -2,19 +2,21 @@
 const nav = document.querySelector('nav');
 
 nav.innerHTML = `
-    <div class="logo">
-        <a href="index.html" class="nomeLogo">SIMULANDO</a>
-    </div>
-    <div class="menu">
-        <div class="ranking">
-            <span class="material-symbols-outlined">
-                trophy
-            </span>
+    <div>
+        <div class="logo">
+            <a href="index.html" class="nomeLogo">SIMULANDO</a>
         </div>
-        <div class="perfil">
-            <span class="material-symbols-outlined">
-                person
-            </span>
+        <div class="menu">
+            <div class="ranking">
+                <span class="material-symbols-outlined">
+                    trophy
+                </span>
+            </div>
+            <div class="perfil">
+                <span class="material-symbols-outlined">
+                    person
+                </span>
+            </div>
         </div>
     </div>`
 
