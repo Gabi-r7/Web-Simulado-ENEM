ac0ed4644273d9d171f7e7aa5b235d4fc68b2c12
Dudisdu99 <131018122+Dudisdud99@users.noreply.github.com>
2024-07-18 22:38:08 +0000

    refatorando nav

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -6,29 +6,55 @@ nav.innerHTML = `
         <div class="logo">
             <a href="/src/tabs/home/index.html" class="nomeLogo">SIMULANDO</a>
         </div>
+
         <div class="menu">
-            <div class="ranking">
+            <div class="nav-op">
+                <span class="material-symbols-outlined">
+                    home
+                </span>
+                <span class="text-icon">Inicio</span>
+            </div>
+            <div class="nav-op">
+                <span class="material-symbols-outlined">
+                    quiz
+                </span>
+                <span class="text-icon">Questões</span>
+            </div>
+            <div class="nav-op">
                 <span class="material-symbols-outlined">
                     trophy
                 </span>
+                <span class="text-icon">Ranking</span>
             </div>
-            <div class="perfil">
+            <div class="nav-op">
+                <span class="material-symbols-outlined">
+                    live_help
+                </span>
+                <span class="text-icon">Suporte</span>
+            </div>
+        </div>
+        
+        <div class="user">
+            <div class="nav-op">
                 <span class="material-symbols-outlined">
                     person
                 </span>
+                <span class="text-icon">Perfil</span>
             </div>
-            <div class="cadastrar">    
+            <div class="nav-op">    
                 <a href="/src/tabs/signUp/cadastro.html">
                     <span class="material-symbols-outlined">
                         person_add
                     </span>
+                    <span class="text-icon">SingUp</span>
                 </a>
             </div>
-            <div class="logout">
+            <div class="nav-op">
                 <button onclick="logout()">
                     <span class="material-symbols-outlined">
-                        exit_to_app
+                        logout
                     </span>
+                    <span class="text-icon">LogOut</span>
                 </button>
             </div>
         </div>
