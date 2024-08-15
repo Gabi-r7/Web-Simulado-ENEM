d0969a5df03a93cdbf637032beea5ff608b742d5
Eduardo Flesch <duduflesch2007@gmail.com>
2024-08-15 09:53:01 -0300

    arrumando modal

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -7,14 +7,25 @@ body.appendChild(modal); // Append 'modal' to 'main'
 modal.id = 'modal';
 modal.style.display = 'none';
 
+const modalModal = document.createElement('div'); // Create the 'modal-modal' element
+modal.appendChild(modalModal); // Append 'modalModal' to 'modal'
+modalModal.classList.add('modal-modal');
+
 const modalContent = document.createElement('div'); // Create the 'modal-content' element
-modal.appendChild(modalContent); // Append 'modalContent' to 'modal'
+modalModal.appendChild(modalContent); // Append 'modalContent' to 'modalModal'
 modalContent.classList.add('modal-content');
+modalContent.classList.add('main-div')
+
+
+const divH1 = document.createElement('div'); // Create the 'divH1' element
+const divH2 = document.createElement('div'); // Create the 'divH2' element
+modalContent.appendChild(divH1); // Append 'divH1' to 'modalContent'
+modalContent.appendChild(divH2); // Append 'divH2' to 'modalContent'
 
 const h1 = document.createElement('h1'); // Create the 'h1' element
 const h2 = document.createElement('h2'); // Create the 'h2' element
-modalContent.appendChild(h1); // Append 'h1' to 'modalContent'
-modalContent.appendChild(h2); // Append 'h2' to 'modalContent'
+divH1.appendChild(h1); // Append 'h1' to 'modalContent'
+divH2.appendChild(h2); // Append 'h2' to 'modalContent'
 
 nav.innerHTML = `
     <div>
@@ -109,7 +120,7 @@ function showModal(responseJson, message) {
     }
     else {
         if (responseJson.status === 'success') {
-            color = 'green';
+            color = 'rgb(7, 255, 20)';
         }
         h1.style.color = color;
         h1.innerHTML = responseJson.status;
